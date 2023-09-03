0dff4c54974ff8c85bdc507987af8c7dbab7571d
MM Lee <lukei321@kakao.com>
2023-09-03 21:24:59 +0900

    template work

--- src/main/java/com/example/community_service/controller/MainController.java
+++ src/main/java/com/example/community_service/controller/MainController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class MainController {
 
     @GetMapping
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/board")
+    public String board() {
+        return "board";
+    }
+
+    @GetMapping("/board/write")
+    public String write() {
+        return "post";
     }
 }
--- src/main/java/com/example/community_service/controller/MemberController.java
+++
@@ -1,8 +0,0 @@
-package com.example.community_service.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MemberController {
-
-}
